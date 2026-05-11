66920f783effa0def1344cc778fcc7722ddbb7d7
DeVelox <DeVelox@users.noreply.github.com>
2026-05-11 14:23:59 +0200

    switch to weekly builds

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,17 +2,11 @@
 
 set -ouex pipefail
 
-# Nightly COSMIC COPR
-# dnf5 -y copr enable ryanabx/cosmic-epoch
-# dnf5 -y install cosmic-desktop
-# dnf5 -y copr disable ryanabx/cosmic-epoch
-
-# systemctl disable gdm.service
-# systemctl enable cosmic-greeter.service
-
-# Extra apps
+# Terra
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf5 -y install niri mako fuse nautilus gnome-keyring
+# Niri
+dnf5 -y install niri mako fuse nautilus gnome-keyring xfce-polkit
+# Apps
 dnf5 -y install atuin alacritty bat btop direnv eza fd-find fish helix ncdu \
 nix-core podlet podman-compose quickemu ripgrep starship ugrep zellij zoxide
 
