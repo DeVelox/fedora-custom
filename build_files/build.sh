a5b9fd5381e238e455cd69ca9c492abd90296af5
DeVelox <DeVelox@users.noreply.github.com>
2026-07-09 18:09:58 +0200

    add fuse-libs

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 # Terra
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 # Niri
-dnf5 -y install niri mako fuse nautilus gnome-keyring xfce-polkit
+dnf5 -y install niri mako nautilus gnome-keyring xfce-polkit fuse-libs
 # Apps
 dnf5 -y install atuin alacritty bat btop direnv eza fd-find fish helix ncdu \
 nix-core podlet podman-compose quickemu ripgrep starship ugrep zellij zoxide
